a1459aca11c735672d91b013fd204ef00f776d28
Michael Tran <Mike@Michaels-MacBook-Air.local>
2015-12-10 13:19:00 -0600

    1st commit
    Capstone Project Fall 2015
    2D platformer written in HTML5 and JavaScript

---
+++ source/js/prefabs/FlyingEnemy.js
@@ -0,0 +1,13 @@
+var Platformer = Platformer || {};
+
+Platformer.FlyingEnemy = function (game_state, position, properties) {
+    "use strict";
+    Platformer.Enemy.call(this, game_state, position, properties);
+    
+    // flying enemies are not affected by gravity
+    this.body.allowGravity = false;
+    
+};
+
+Platformer.FlyingEnemy.prototype = Object.create(Platformer.Enemy.prototype);
+Platformer.FlyingEnemy.prototype.constructor = Platformer.FlyingEnemy;
